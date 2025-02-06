689289a014f281ac911f194a7955bc19a2c93c0b
Elkis243 <elielubama@gmail.com>
2025-02-07 00:03:21 +0100

    Amelioration de l'interface

--- renouveau_urbain/settings/production.py
+++ renouveau_urbain/settings/production.py
@@ -1,10 +1,9 @@
 from .base import *
 
-SECRET_KEY = 'U6cfo0sw8kG13P_LykDnNdZC-896W9rJRA7AYYvxvPKkee4NFmMV_tW7BrYK1i23nyw'
-#ALLOWED_HOSTS = ['genytelecom.pythonanywhere.com']
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = "U6cfo0sw8kG13P_LykDnNdZC-896W9rJRA7AYYvxvPKkee4NFmMV_tW7BrYK1i23nyw"
+ALLOWED_HOSTS = ["elkis.pythonanywhere.com"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
@@ -13,9 +12,8 @@ SECURE_SSL_REDIRECT = True
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-#CSRF_TRUSTED_ORIGINS = ['https://genytelecom.pythonanywhere.com']
-CSRF_TRUSTED_ORIGINS = ['*']
+CSRF_TRUSTED_ORIGINS = ["https://elkis.pythonanywhere.com"]
 
-X_FRAME_OPTIONS = 'DENY'
+X_FRAME_OPTIONS = "DENY"
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
